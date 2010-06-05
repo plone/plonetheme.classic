f83f768cca6c801d222a4b219fc82bde2db690a7
David Glick <davidglick@groundwire.org>
2010-06-05 05:15:19 +0000

    fix the theme upgrade so that Plone Default from Plone 3 ends up as either Plone Classic Theme (if it was uncustomized) or 'Old Plone 3 Custom Theme' (if it was), and with correct viewlets. fixes #10399
    svn path=/plonetheme.classic/trunk/; revision=37005

--- plonetheme/classic/browser/interfaces.py
+++ plonetheme/classic/browser/interfaces.py
@@ -5,3 +5,9 @@ class IThemeSpecific(IDefaultPloneLayer):
        If you need to register a viewlet only for the
        "Plone Classic Theme" theme, this interface must be its layer.
     """
+
+class ILegacyThemeSpecific(IThemeSpecific):
+    """Marker interface for the "Old Plone 3 Custom Theme" skin that is
+       created when upgrading the "Plone Default" skin from Plone 3, to make
+       sure it continues to get the old viewlet order.
+    """
